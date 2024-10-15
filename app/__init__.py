2b337d088f7fa40c9c5309514a24f21bee3d0cc0
Chris Abdo <chris.j.abdo@gmail.com>
2024-10-15 19:50:33 -0400

    test

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,20 @@ import importlib
 from app.commands import CommandHandler
 from app.commands import Command
 
+
 class App:
     def __init__(self):
         self.command_handler = CommandHandler()
 
     def load_plugins(self):
-        plugins_package = 'app.plugins'
-        for _, plugin_name, is_pkg in pkgutil.iter_modules([plugins_package.replace('.', '/')]):
+        plugins_package = "app.plugins"
+        for _, plugin_name, is_pkg in pkgutil.iter_modules(
+            [plugins_package.replace(".", "/")]
+        ):
             if is_pkg:
-                plugin_module = importlib.import_module(f'{plugins_package}.{plugin_name}')
+                plugin_module = importlib.import_module(
+                    f"{plugins_package}.{plugin_name}"
+                )
                 for item_name in dir(plugin_module):
                     item = getattr(plugin_module, item_name)
                     try:
@@ -21,16 +26,18 @@ class App:
                         continue
 
     def start(self):
-       self.load_plugins()
-       while True:
-           input_line = input(">>> ").strip()
-           if not input_line:
-               continue
-           parts = input_line.split()
-           command_name = parts[0].lower()
-           args = parts[1:]
-           if command_name == 'exit':
-               print("Goodbye!")
-               break
-           else:
-               self.command_handler.execute_command(command_name, args)
+        self.load_plugins()
+        self.command_handler.execute_command("menu", [])
+
+        while True:
+            input_line = input(">>> ").strip()
+            if not input_line:
+                continue
+            parts = input_line.split()
+            command_name = parts[0].lower()
+            args = parts[1:]
+            if command_name == "exit":
+                print("Goodbye!")
+                break
+            else:
+                self.command_handler.execute_command(command_name, args)
